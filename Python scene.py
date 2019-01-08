848ef0439a587c5ec83be1ec08cdc7df1566bf90
110108 <31996737+110108@users.noreply.github.com>
2019-01-08 08:27:07 -0500

    blin

---
+++ Python scene.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+
+turtle.colormode(255)
+turtle.speed(0)
+turtle.tracer(0)
+turtle.ht()
+turtle.penup()
+
+def drawBus(x,y,length,color):
+    turtle.goto(x,y)
+    turtle.pendown()
+    
