a8f9eeb7dd94aa886710f3c2842651fda9fce5fc
110108 <31996737+110108@users.noreply.github.com>
2019-01-09 08:26:53 -0500

    Update Python scene.py

--- Python scene.py
+++ Python scene.py
@@ -7,7 +7,27 @@ turtle.tracer(0)
 turtle.ht()
 turtle.penup()
 
-def drawBus(x,y,length,color):
+def curve(angle, size):
+    turtle.pd()
+    for i in range(0,angle,1):
+        turtle.forward(size/angle)
+        turtle.left(1)
+    turtle.pu()
+
+def drawBus(x,y,scale):
     turtle.goto(x,y)
     turtle.pendown()
+    turtle.begin_fill()
+    turtle.color(167,169,172)
+    turtle.forward(10*scale)
+    curve(91,(scale/2))
+    turtle.forward(5*scale)
+    pos=
+    curve(90,(scale/2))
+    turtle.forward(10*scale)
+    curve(88,(scale/2))
+    turtle.forward(5*scale)
+    curve(90,(scale/2))
+    turtle.end_fill()
     
+drawBus(-70,0,50)
