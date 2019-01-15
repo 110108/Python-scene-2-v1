3cfd2761e8dfe6c92051ffd08fb1bdbdffc8d148
110108 <31996737+110108@users.noreply.github.com>
2019-01-15 08:27:09 -0500

    Update Python scene.py

--- Python scene.py
+++ Python scene.py
@@ -8,15 +8,30 @@ turtle.ht()
 turtle.penup()
 
 def curve(angle, size):
-    turtle.pd()
     for i in range(0,angle,1):
         turtle.forward(size/angle)
         turtle.left(1)
-    turtle.pu()
 
 def drawBus(x,y,scale):
+    #bus wheels
+    #wheel 1
+    turtle.goto(x-(scale*160),y+(scale*12))
+    turtle.pd()
+    turtle.seth(90)
+    turtle.begin_fill()
+    turtle.circle(scale*50)
+    turtle.end_fill()
+    #wheel 2
+    turtle.goto(x-(scale*860),y+(scale*12))
+    turtle.pd()
+    turtle.seth(90)
+    turtle.begin_fill()
+    turtle.circle(scale*50)
+    turtle.end_fill()
+    
     #bus body
     turtle.goto(x,y)
+    turtle.seth(0)
     turtle.pendown()
     turtle.begin_fill()
     turtle.color(167,169,172)
@@ -77,13 +92,51 @@ def drawBus(x,y,scale):
     turtle.end_fill()
     turtle.pu()
     
-    #windows
+    #window #1
     turtle.goto(win)
     turtle.seth(180)
     turtle.forward(70*scale)
+    turtle.color(66, 150, 255)
     turtle.pd()
+    turtle.begin_fill()
+    turtle.forward((scale*50)*5)
+    win=turtle.pos()
+    curve(90,(scale*50))
+    turtle.forward((50*scale)*3)
+    curve(90,(scale*50))
     turtle.forward((scale*50)*5)
     curve(90,(scale*50))
+    turtle.forward((50*scale)*3)
+    curve(90,(scale*50))
+    turtle.end_fill()
+    turtle.pu()
+    
+    #window #2
+    turtle.goto(win)
+    turtle.seth(180)
+    turtle.forward(150*scale)
+    turtle.color(66, 150, 255)
+    turtle.pd()
+    turtle.begin_fill()
+    turtle.forward((scale*50)*5)
+    win=turtle.pos()
+    curve(90,(scale*50))
+    turtle.forward((50*scale)*3)
+    curve(90,(scale*50))
+    s=turtle.pos()
+    turtle.forward((scale*50)*5)
+    curve(90,(scale*50))
+    turtle.forward((50*scale)*3)
+    curve(90,(scale*50))
+    turtle.end_fill()
+    turtle.pu()
+    
+    turtle.goto(s)
+    turtle.seth(180)
+    turtle.forward(scale*180)
+    turtle.left(88)
+    turtle.pd()
+    turtle.forward(100)
     
 drawBus(100,10,.5)
 turtle.update()
